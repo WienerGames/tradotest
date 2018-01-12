7b571ee1666f941a87bc678ce8c4372b92bf652e
Angel Dikov <herrdikov@gmail.com>
2018-01-12 14:29:24 +0200

    init

---
+++ airbridge.js
@@ -0,0 +1,29 @@
+var AirBridge = (function() {
+	var instance;
+	function createInstance() {
+		var object = new Object();
+		object.useWindowLocation = true;
+		return object;
+	}
+	
+	return {
+		setUseWindowLocation: function( $shouldUseWindowLocation ) {
+			if (!instance) {
+				instance = createInstance();
+			}
+			instance.useWindowLocation = $shouldUseWindowLocation;	
+		},
+		
+		message: function( $message ) {
+			if (!instance) {
+				instance = createInstance();
+			}
+			if (!instance.useWindowLocation) {
+				NativeWebView.airBridge( $message );
+			}
+			else {
+				window.location = "airBridge:" + $message;
+			}
+		}
+	};
+})();
